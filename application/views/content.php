7e27bd9223d3841bb2306d84bd9160a996d372e8
Rizky Said <rizkysaid40@gmail.com>
2019-05-31 11:26:25 +0700

    rizky said

--- application/views/content.php
+++ application/views/content.php
@@ -30,6 +30,15 @@
 		      </div>
 		    </div>
 		</div>
+
+		<div class="col-sm-12 col-md-4">
+		    <div class="thumbnail">
+		      <a href="javascript:void();" onclick="get(3);"><img src="<?php echo base_url() ?>assets/img/cukup_puas.png" width="150" alt="Cukup Puas"></a>
+		      <div class="caption">
+		        <h4>Ini Link Rizky Said</h4>
+		      </div>
+		    </div>
+		</div>
 	</div>
 
 
